e11e6cbd843297af082b418975d23fc70a3c717e
lyn <3228562873@qq.com>
2024-12-09 21:39:02 +0800

    基础增删改查

---
+++ src/main/java/com/lyn/mianshiya/mapper/QuestionBankMapper.java
@@ -0,0 +1,18 @@
+package com.lyn.mianshiya.mapper;
+
+import com.lyn.mianshiya.model.entity.QuestionBank;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author lyn
+* @description 针对表【question_bank(题库)】的数据库操作Mapper
+* @createDate 2024-12-09 18:09:36
+* @Entity com.lyn.mianshiya.model.entity.QuestionBank
+*/
+public interface QuestionBankMapper extends BaseMapper<QuestionBank> {
+
+}
+
+
+
+
